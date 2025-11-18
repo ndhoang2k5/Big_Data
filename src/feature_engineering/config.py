0866450ed7851925ffec1ef52cf2a39428254f87
Nguyen Duy Hoang <nguyenduyhoang0511250@gmail.com>
2025-11-18 19:13:42 +0700

    refix config

--- src/feature_engineering/config.py
+++ src/feature_engineering/config.py
@@ -1,4 +1,7 @@
+# stock_price_prediction/src/feature_engineering/config.py
+
 # Kafka Configuration
+# SỬ DỤNG kafka:9092 vì PySpark Processor chạy bên trong container Docker
 KAFKA_BROKER_SERVERS = "kafka:9092"
 KAFKA_RAW_DATA_TOPIC = "stock_raw_data"
 KAFKA_FEATURES_TOPIC = "stock_features_data"
@@ -10,4 +13,5 @@ SPARK_APP_NAME = "StockFeatureEngineering"
 BATCH_INTERVAL_SECONDS = 5
 
 # Đường dẫn lưu checkpoint của Spark Structured Streaming
+# Phải là đường dẫn bên trong container
 CHECKPOINT_LOCATION = "/tmp/spark_checkpoint"
