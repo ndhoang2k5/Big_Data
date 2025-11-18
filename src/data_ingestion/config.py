08fac7a99f657850b779c2c6f104ea809dd96690
Nguyen Duy Hoang <nguyenduyhoang0511250@gmail.com>
2025-11-18 19:56:35 +0700

    fix data_ingestion/config

--- src/data_ingestion/config.py
+++ src/data_ingestion/config.py
@@ -4,7 +4,7 @@
 # SỬ DỤNG localhost:9093 vì Producer chạy trên Host và Kafka expose qua port 9093
 KAFKA_BROKER = "localhost:9093"  
 KAFKA_TOPIC = "stock_raw_data"
-MESSAGE_RATE_PER_SECOND = 500
+MESSAGE_RATE_PER_SECOND = 250
 
 # Simulated Stock API Configuration
 STOCK_SYMBOLS = ["VCB", "HPG", "FPT", "VIC", "VNM", "MWG", "PNJ", "SSI", "TPB", "POW"]
