645834282675a0229774f8f6e73dcb83711e31c4
Nguyen Duy Hoang <nguyenduyhoang0511250@gmail.com>
2025-11-18 19:13:28 +0700

    fix config

--- src/data_ingestion/config.py
+++ src/data_ingestion/config.py
@@ -1,9 +1,10 @@
 # stock_price_prediction/src/data_ingestion/config.py
 
 # Kafka Configuration
-KAFKA_BROKER = "localhost:9093"  # Port 9093 là port chúng ta expose ra ngoài từ docker-compose
+# SỬ DỤNG localhost:9093 vì Producer chạy trên Host và Kafka expose qua port 9093
+KAFKA_BROKER = "localhost:9093"  
 KAFKA_TOPIC = "stock_raw_data"
-MESSAGE_RATE_PER_SECOND = 500 # Giả lập 500 message/giây
+MESSAGE_RATE_PER_SECOND = 500
 
 # Simulated Stock API Configuration
 STOCK_SYMBOLS = ["VCB", "HPG", "FPT", "VIC", "VNM", "MWG", "PNJ", "SSI", "TPB", "POW"]
