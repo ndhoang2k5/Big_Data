0fd3d7d1af876e3c0b8985857a47eb35faaf0f8d
Nguyen Duy Hoang <nguyenduyhoang0511250@gmail.com>
2025-11-19 14:59:31 +0700

    update config

--- src/data_ingestion/config.py
+++ src/data_ingestion/config.py
@@ -1,11 +1,9 @@
 # stock_price_prediction/src/data_ingestion/config.py
 
 # Kafka Configuration
-# CHỈNH SỬA: SỬ DỤNG TÊN SERVICE CỦA KAFKA TRONG DOCKER COMPOSE NETWORK.
-# Mặc định là 'kafka' và port là 9092.
+# SỬ DỤNG CỔNG BÊN NGOÀI (HOST) CHO PRODUCER (docker-compose ánh xạ 9092:9092)
 KAFKA_BROKER = "localhost:9092" 
 KAFKA_TOPIC = "stock_raw_data"
-MESSAGE_RATE_PER_SECOND = 500
+MESSAGE_RATE_PER_SECOND = 200
 
-# Simulated Stock API Configuration
 STOCK_SYMBOLS = ["VCB", "HPG", "FPT", "VIC", "VNM", "MWG", "PNJ", "SSI", "TPB", "POW"]
